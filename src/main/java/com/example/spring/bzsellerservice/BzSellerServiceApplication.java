22266077f6076e4b6765a79c21e84c028fc42785
Ninky0 <2020113392@dgu.ac.kr>
2025-01-10 15:55:10 +0900

    feat : s3 config, docker

--- src/main/java/com/example/spring/bzsellerservice/BzSellerServiceApplication.java
+++ src/main/java/com/example/spring/bzsellerservice/BzSellerServiceApplication.java
@@ -1,12 +1,23 @@
 package com.example.spring.bzsellerservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BzSellerServiceApplication {
-
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
     public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Set system properties for AWS keys
+        System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
+        System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+
+
         SpringApplication.run(BzSellerServiceApplication.class, args);
     }
 
