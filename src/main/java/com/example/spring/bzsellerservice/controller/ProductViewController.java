fafe87859d0d232bab6b3ebf543eb6b93983ddf7
khp0916 <khlove0916@naver.com>
2025-01-15 17:19:36 +0900

    01/15 product

--- src/main/java/com/example/spring/bzsellerservice/controller/ProductViewController.java
+++ src/main/java/com/example/spring/bzsellerservice/controller/ProductViewController.java
@@ -1,34 +1,72 @@
 package com.example.spring.bzsellerservice.controller;
 
 import com.example.spring.bzsellerservice.dto.product.ProdReadResponseDTO;
-
-import com.example.spring.bzsellerservice.service.SellerService;
+import com.example.spring.bzsellerservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductViewController {
 
-    private final SellerService sellerService;
+    private final ProductService productService;
+
+    // 판매자가 판매하는 상품들
+    @GetMapping
+    public String productList(@RequestParam(defaultValue = "0") int page, Model model) {
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by("id").ascending());
+
+        Page<ProdReadResponseDTO> productPage = productService.findAll(pageable);
+        List<ProdReadResponseDTO> products = productPage.getContent();
+
+        int totalPages = productPage.getTotalPages();
+        int pageBlock = 10; // 페이지 블록 크기
+        int startPage = (page / pageBlock) * pageBlock;
+        int endPage = Math.min(startPage + pageBlock - 1, totalPages - 1);
+
+        model.addAttribute("products", products);
+        model.addAttribute("productPage", productPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("showPrevious", startPage > 0);
+        model.addAttribute("showNext", endPage < totalPages - 1);
+
+        return "product_list";
+    }
 
     @GetMapping("/upload")
     public String uploadProduct() {
         //상품 등록 폼으로 이동
-        return "product_upload";
+        return "upload_product";
+    }
+
+    // admin 상품 상세 정보 매핑 추가
+    @GetMapping("/detail/{id}")
+    public String productDetail(@PathVariable Long id, Model model) {
+        ProdReadResponseDTO product = productService.getProductDetails(id);
+        model.addAttribute("product", product);
+        return "detail_product"; // 상세 페이지 HTML 파일 이름 반환
     }
 
     @GetMapping("/edit/{id}")
     public String editProduct(@PathVariable Long id, Model model) {
         // 상품의 상세 정보를 조회하여 수정 폼에 표시할 수 있도록 모델에 추가
-        ProdReadResponseDTO product = sellerService.getProductDetails(id);
+        ProdReadResponseDTO product = productService.getProductDetails(id);
         model.addAttribute("product", product);
-        return "product_edit"; // 수정 페이지 HTML 파일 이름
+        return "edit_product"; // 수정 페이지 HTML 파일 이름
     }
 
 }
