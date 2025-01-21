418fe2a826c582216a995be13c9c38a2a3f8b661
dlghdu <fhrmsl12@gmail.com>
2025-01-21 16:45:23 +0900

    feat: seller mymarket

--- src/main/java/com/example/spring/bzsellerservice/service/ImgServiceImpl.java
+++ src/main/java/com/example/spring/bzsellerservice/service/ImgServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.spring.bzsellerservice.service;
 
 import com.example.spring.bzsellerservice.config.s3.S3Uploader;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 public class ImgServiceImpl implements ImgService {
     private final S3Uploader s3Uploader;
@@ -21,6 +23,7 @@ public class ImgServiceImpl implements ImgService {
             throw new IllegalArgumentException("MultipartFile must not be null or empty.");
         } else {
             url = s3Uploader.uploadFileToS3(file, "static/bz-product/");
+            log.info("[ImgServiceImpl] Uploaded image URL: {}", url); // 로그 추가
             return url;
         }
     }
