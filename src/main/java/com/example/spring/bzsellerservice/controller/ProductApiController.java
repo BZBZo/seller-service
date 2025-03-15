cdf49f641aa5332af15cf50f5a1e3e9919a06071
Ninky0 <2020113392@dgu.ac.kr>
2025-03-16 00:25:16 +0900

    fix : merge duplicate function by using one endpoint

--- src/main/java/com/example/spring/bzsellerservice/controller/ProductApiController.java
+++ src/main/java/com/example/spring/bzsellerservice/controller/ProductApiController.java
@@ -74,28 +74,6 @@ public class ProductApiController {
         return productPage;
     }
 
-    @GetMapping("/edit/{id}")
-    public ResponseEntity<ProdReadResponseDTO> editProduct(@PathVariable Long id) {
-        // 상품의 상세 정보를 조회
-        ProdReadResponseDTO product = sellerService.getProductDetails(id);
-        return ResponseEntity.ok(product); // JSON 형식으로 반환
-    }
-
-    @GetMapping("/detail/{id}")
-    public ResponseEntity<ProdReadResponseDTO> getProductDetail(@PathVariable Long id) {
-        // 로그 추가
-        System.out.println("Seller Controller 호출: 상품 ID = " + id);
-
-        // 서비스 계층에서 상품 정보 가져오기
-        ProdReadResponseDTO product = sellerService.getProductDetails(id);
-
-        if (product == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(product);
-    }
-
     @PostMapping
     public ResponseEntity<ProdUploadResponseDTO> addProduct(
             @ModelAttribute ProdUploadRequestDTO dto,
@@ -140,6 +118,21 @@ public class ProductApiController {
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<ProdReadResponseDTO> getProductDetail(@PathVariable Long id) {
+        // 로그 추가
+        System.out.println("Seller Controller 호출: 상품 ID = " + id);
+
+        // 서비스 계층에서 상품 정보 가져오기
+        ProdReadResponseDTO product = sellerService.getProductDetails(id);
+
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(product);
     }
 
     @PutMapping("/{id}")
